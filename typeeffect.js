ca72c05c463acad0d8a70d87f917000dc0dae2ea
lingjing0801 <dapaojing@gmail.com>
2019-09-26 19:02:21 +0800

    time

--- typeeffect.js
+++ typeeffect.js
@@ -1,5 +1,5 @@
 var texts = ["好嗎?","不好，感冒了","好累"]; // 陣列
-var speed = 1000                           // 速度
+var speed = 600                           // 速度
 var index = 0;                             // 編號
 var count = 0;                             // 字數
 var id = "type-box";                       //要寫入文字的ID
@@ -9,9 +9,11 @@ var id = "type-box";                       //要寫入文字的ID
 function type(){
     var box=document.getElementById(id);
     // 變數 切割完的文字 = 文字陣列 切割文字 (開始位置,切幾個字)
-    var slice = texts[index].slice(0,1);
+    var slice = texts[index].slice(0,count++);
     // 盒子.插入內容 = 切割完的文字
     box.innerHTML=slice;
+    // 設定重複時間 (方法名稱,間格時間(毫秒))
+    setTimeout(type,speed);
 }
 // 當網頁載入完成時執行方法 type
 document.addEventListener("DOMContentLoaded",type);
